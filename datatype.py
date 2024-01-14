0c0f7751c023ed3fe991ce5ede4bc0049c467d51
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-14 17:03:51 +0530

    Data types in python

---
+++ datatype.py
@@ -0,0 +1,24 @@
+# data types in python
+
+# 1. int == integer
+a = 10
+print(type(a))  # <class 'int'>
+
+# 2. str
+b = "Saurabh"  
+print(type(b))   # <class 'str'>
+
+# 3. Float
+c = 1.05
+print(type(c))  # <class 'float'>
+
+# 4. Boolean
+d = True
+e = False
+print(type(d))  # <class 'bool'>
+print(type(e))  # <class 'bool'>
+
+
+# 5. None === empty
+f = None
+print(type(f))  # <class 'NoneType'>
