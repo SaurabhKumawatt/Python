8a5830017b05a06c0f2c8ec545c5bcaebdbefb12
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-14 17:07:02 +0530

    PIP in python

---
+++ pip.py
@@ -0,0 +1,14 @@
+# Python PIP
+# PIP is a package manager for Python packages, or modules if you like.
+
+'''
+What is a Package?
+A package contains all the files you need for a module.
+
+Modules are Python code libraries you can include in your project.
+'''
+
+# Check if PIP is Installed ---(pip --version)
+# Download a Package -- (pip install value)
+# Remove a Package -- (pip uninstall value)
+#  list of installed package -- (pip list)
