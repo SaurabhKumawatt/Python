ec839d91d2683ff4c8b6c7489a3eb216c5c85bdb
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-14 17:06:47 +0530

    User input in python

---
+++ user_input.py
@@ -0,0 +1,31 @@
+# User Input
+'''
+Python allows for user input
+'''
+
+# username = input("Enter a username: ")
+# print(username)
+
+# input type is by default string
+# num = input("Enter a number: ")
+# print(num)
+# print(type(num))
+
+
+# typecasting input
+# str to int
+# num = int(input("Enetr a number: "))
+# print(num)
+# print(type(num))
+
+# str to float
+# num = float(input("enter a number: "))
+# print(num)
+# print(type(num))
+
+# str to boolean
+boolean = bool(input("Enter a value: "))
+print(boolean)
+print(type(boolean))
+# all values return true
+# no value === false ("" == false)
