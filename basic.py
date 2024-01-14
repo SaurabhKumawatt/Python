050f80e4aab5985e05753ef77eb5a2ddb3751d2b
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-14 13:09:25 +0530

    basic of Python

---
+++ basic.py
@@ -0,0 +1,22 @@
+# Hello world to master
+print("Hello, World!")
+
+# check python version in terminal --- python --version
+# indentation: is important
+
+
+############################################
+
+# comments in python
+
+# single line comment
+# i am comment and i am not excute :(
+
+# multi line comment
+'''
+Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code.
+'''
